b4d062a6097f26a30d7a752d9e5f6c5a42f6f550
CitrusVII <maryamkar0052@gmail.com>
2025-06-28 15:28:51 +0100

    added supabase client

--- package/src/lib/supabaseClient.ts
+++ package/src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL (LOL)!;
+// Make sure to install '@supabase/supabase-js' with: npm install @supabase/supabase-js
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
